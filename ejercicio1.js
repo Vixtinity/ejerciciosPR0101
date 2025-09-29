79fe26bc467e193f85670a52720cf74beb55d2c5
Ismael <08062006ismael@gmail.com>
2025-09-29 18:48:56 +0200

    first commit

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+let numero = 1;
+let string = "Hola adios";
+let verdadero = true;
+let falso = false;
+let arrayytal = ["Dame un que", "un grrrr"];
+
+
+console.log(typeof(numero),numero);
+console.log(typeof(string), string);
+console.log(typeof(verdadero), verdadero);
+console.log(typeof(falso), falso);
+console.log(typeof(arrayytal), arrayytal);
---
+++ ejercicio2.js
@@ -0,0 +1,7 @@
+var array100 = [];
+
+for (var i = 1; i<= 100; i++){
+    array100.push(i);
+}
+
+console.table(array100)
