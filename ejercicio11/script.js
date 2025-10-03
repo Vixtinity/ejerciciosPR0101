5671985ec581cae76047329d86c00c64a9ec7a17
Ismael <08062006ismael@gmail.com>
2025-10-03 17:11:43 +0200

    ej11 e imagen

--- ejercicio11/script.js
+++ ejercicio11/script.js
@@ -1,7 +1,13 @@
 function ejercicio11() {
-    let numeros = [];
-    for (let i = 0; i < 100; i++) {
-        let numerosaleatorios = Math.floor(Math.random() * 20) + 1; // 1 a 20
-        numeros.push(numerosaleatorios);
-    }
+  let numeros = [];
+    //voy a poner que genere 20 para que se note mejor, con 100 casi siempre sale 20.
+  for (let i = 0; i < 20; i++) {
+    let n = Math.floor(Math.random() * 20) + 1;
+    numeros.push(n);
+  }
+
+  let unicos = new Set(numeros);
+
+  document.getElementById("numerogenerados").innerText = "Números: " + numeros;
+  document.getElementById("resultado").innerText = "Diferentes: " + unicos.size;
 }
