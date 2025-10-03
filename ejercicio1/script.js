0599bd6ec46585d5d5869e1b3d6ce186c45b5023
Ismael <08062006ismael@gmail.com>
2025-10-03 17:29:45 +0200

    arreglos

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,18 +1,17 @@
 function ejercicio1() {
-let numero = 1;
-let string = "Hola adios";
+  let numero = 1;
+  let string = "Hola adios";
 
-let verdadero = true;
-let falso = false;
-let arrayprueba = ["Prueba1", "Prueba2"];
+  let verdadero = true;
+  let falso = false;
+  let arrayprueba = ["Prueba1", "Prueba2"];
 
-
-console.log(typeof(numero),numero);
-console.log(typeof(string), string);
-console.log(typeof(verdadero), verdadero);
-console.log(typeof(falso), falso);
-console.log(typeof(arrayprueba), arrayprueba);
-document.getElementById("comprueba").innerText = "Comprueba la terminal";
+  console.log("Numero: ", typeof(numero), numero);
+  console.info("Cadena:", typeof(string), string);
+  console.error("Boolean true:", typeof(verdadero), verdadero);
+  console.debug("Boolean false:", typeof(falso), falso);
+  console.warn("Array:", typeof(arrayprueba), arrayprueba);
+  document.getElementById("comprueba").innerText = "Comprueba la consola del navegador";
 }
 
 ejercicio1();
